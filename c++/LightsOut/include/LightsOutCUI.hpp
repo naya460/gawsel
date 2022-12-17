9bcfce76a8447932b6dba7f3aa91e2c13437860f
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-17 18:20:58 +0900

    LightsOutCUIの中にゲームプレイ用関数を用意

--- c++/LightsOut/include/LightsOutCUI.hpp
+++ c++/LightsOut/include/LightsOutCUI.hpp
@@ -1,11 +1,21 @@
 #pragma once
 
 #include <cstdint>
+#include <string>
 
 #include "LightsOutUI.hpp"
 
 class LightsOutCUI : public LightsOutUI {
+private:
+    // 値が範囲内か調べる
+    bool Inside(std::uint16_t min, std::uint16_t target, std::uint16_t max) noexcept;
+
+    // 制限付きの値を入力
+    std::uint16_t Input(std::string str, std::uint16_t min, std::uint16_t max);
 public:
+    // ゲームのメインループを実行
+    void Run() noexcept;
+
     // 新しい盤面で開始
     void NewGame(std::uint8_t length) noexcept override;
 
--- c++/LightsOut/src/LightsOutCUI.cpp
+++ c++/LightsOut/src/LightsOutCUI.cpp
@@ -2,13 +2,63 @@
 
 #include <iostream>
 
-// 新しい盤面で開始
+bool LightsOutCUI::Inside(std::uint16_t min, std::uint16_t target, std::uint16_t max) noexcept{
+    return min <= target && target <= max;
+}
+
+std::uint16_t LightsOutCUI::Input(std::string str, std::uint16_t min, std::uint16_t max){
+    int num;
+    while (true) {
+        std::cout << str;
+        std::cin >> num;
+        // 範囲内のときループを抜ける
+        if (Inside(min, num, max)) break;
+        // 範囲外のとき
+        std::cout << "Try again..." << std::endl;
+    }
+    return num;
+}
+
+void LightsOutCUI::Run() noexcept{
+    // メインループ
+    while (true) {
+        // 大きさを聞く
+        unsigned int length = 3;
+        length = Input("Input board length (2 - 255) : ", 2, 255);
+
+        // 新しい盤面を開始
+        NewGame(length);
+        std::cout << "Game : " << length << "x" << length << std::endl;
+
+        // クリアまで入力と表示を繰り返す
+        while (!IsSuccess()) {
+            // 出力
+            Display();
+
+            // 入力
+            unsigned int row = 0, column = 0;
+            
+            std::string row_str = "Input row (0-" + std::to_string(length - 1) + ") : ";
+            row = Input(row_str, 0, length - 1);
+
+            std::string column_str = "Input column (0-" + std::to_string(length - 1) + ") : ";
+            column = Input(column_str, 0, length - 1);
+            
+            Push(row, column);
+        }
+        // もう一度遊ぶか聞く
+        bool exit = true;
+        std::cout << "Clear" << std::endl << "0:continue 1:exit" << std::endl << ">>> ";
+        std::cin >> exit;
+        if (exit == true) break;
+    }
+}
+
 void LightsOutCUI::NewGame(std::uint8_t length) noexcept{
     system.set_length(length);
     system.random();
 }
 
-// 盤面を表示
 void LightsOutCUI::Display() noexcept{
     // 大きさを取得
     std::uint8_t length = system.get_length();
@@ -23,7 +73,6 @@ void LightsOutCUI::Display() noexcept{
     }
 }
 
-// ライトを押す
 bool LightsOutCUI::Push(std::uint16_t position) noexcept{
     try {
         system.push(position);
