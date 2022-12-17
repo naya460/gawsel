54888e3dd06848d2298256050c190c8a65b9c808
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-17 11:58:23 +0900

    LightsOutSysでライトを押す

--- c++/LightsOut/src/LightsOut.cpp
+++ c++/LightsOut/src/LightsOut.cpp
@@ -7,6 +7,17 @@ private:
     std::uint8_t length = 0;
     std::uint16_t size = 0;
     std::vector<bool> board;
+
+    void invert(std::uint16_t pos){
+        if (pos >= get_size()) {
+            throw false;
+        }
+        board[pos] = ~board[pos];
+    }
+
+    void invert(std::uint8_t row, std::uint8_t column){
+        invert(column * length + row);
+    }
 public:
     LightsOutSys(std::uint8_t length){
         this->length = length;
@@ -36,10 +47,23 @@ public:
     bool is_on(std::uint8_t row, std::uint8_t column){
         return is_on(column * length + row);
     }
+
+    void push(std::uint16_t pos){
+        push(pos % length, pos / length);
+    }
+
+    void push(std::uint8_t row, std::uint8_t column){
+        invert(row, column);
+        if (row > 0) invert(row - 1, column);
+        if (row < length - 1) invert(row + 1, column);
+        if (column > 0) invert(row, column - 1);
+        if (column < length - 1) invert(row, column + 1);
+    }
 };
 
 int main(void){
     LightsOutSys sys(3);
+    sys.push(1, 1);
     for (int i = 0; i < sys.get_size(); ++i) {
         std::cout << sys.is_on(i) << " ";
         if ((i + 1) % sys.get_length() == 0) {
