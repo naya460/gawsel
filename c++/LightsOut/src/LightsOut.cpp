8fdac4ae24dd1ea19916b9eedcf01a0c7b623fa6
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-17 11:19:19 +0900

    LightsOutSysでライトの状態を取得

--- c++/LightsOut/src/LightsOut.cpp
+++ c++/LightsOut/src/LightsOut.cpp
@@ -5,29 +5,46 @@
 class LightsOutSys {
 private:
     std::uint8_t length = 0;
+    std::uint16_t size = 0;
     std::vector<bool> board;
 public:
     LightsOutSys(std::uint8_t length){
         this->length = length;
+        this->size = length * length;
 
-        board.resize(length * length);
+        board.resize(size);
         for (auto v : board) {
             v = 0;
         }
     }
 
-    std::uint8_t get_length(){
+    std::uint8_t get_length() noexcept{
         return length;
     }
 
-    std::uint8_t get_size(){
+    std::uint16_t get_size() noexcept{
         return length * length;
+    }
+
+    bool is_on(std::uint16_t pos){
+        if (pos >= get_size()) {
+            throw false;
+        }
+        return board[pos];
+    }
+
+    bool is_on(std::uint8_t row, std::uint8_t column){
+        return is_on(column * length + row);
     }
 };
 
 int main(void){
     LightsOutSys sys(3);
-    std::cout << static_cast<size_t>(sys.get_length()) << std::endl;
-    std::cout << static_cast<size_t>(sys.get_size()) << std::endl;
+    for (int i = 0; i < sys.get_size(); ++i) {
+        std::cout << sys.is_on(i) << " ";
+        if ((i + 1) % sys.get_length() == 0) {
+            std::cout << std::endl;
+        }
+    }
     return 0;
 }
