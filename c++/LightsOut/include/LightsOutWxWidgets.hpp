21ddf917567febfa5f53e38b9dd375c6eb35dec7
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-17 23:29:58 +0900

    LightsOutWxWidgetsでボタンを表示

--- c++/LightsOut/include/LightsOutWxWidgets.hpp
+++ c++/LightsOut/include/LightsOutWxWidgets.hpp
@@ -4,6 +4,36 @@
 
 #include "LightsOutUI.hpp"
 
+class LightsOutWxWidgets : public LightsOutUI {
+private:
+    const std::uint8_t max_len = 10;
+    const std::uint16_t max_size = max_len * max_len;
+
+    wxFrame *frame;
+
+    // ボタンの大きさを調整
+    void Fit();
+public:
+    // コンストラクタ
+    LightsOutWxWidgets();
+
+    // デストラクタ
+    ~LightsOutWxWidgets();
+
+    // 新しい盤面で開始
+    void NewGame(std::uint8_t length) noexcept override;
+
+    // 盤面を表示
+    void Display() noexcept override;
+
+    // ライトを押す（失敗時：false）
+    bool Push(std::uint16_t position) noexcept override;
+    bool Push(std::uint8_t row, std::uint8_t column) noexcept override;
+
+    // すべてoffにできているか確認
+    bool IsSuccess() noexcept override;
+};
+
 class LightsOutApp : public wxApp {
 public:
     bool OnInit();
--- c++/LightsOut/src/LightsOutWxWidgets.cpp
+++ c++/LightsOut/src/LightsOutWxWidgets.cpp
@@ -1,7 +1,71 @@
 #include "LightsOutWxWidgets.hpp"
 
-bool LightsOutApp::OnInit(){
-    wxFrame *frame = new wxFrame(NULL, wxID_ANY, wxT("Lights Out"));
+#include <wx/wx.h>
+
+void LightsOutWxWidgets::Fit(){
+    // サイズを計算
+    int x, y, len;
+    frame->GetClientSize(&x, &y);
+    len = std::min(x, y);
+    len = std::max(50, len / system.get_length());
+
+    // ボタンのサイズをを変更
+    for (int i = 0; i < max_size; ++i) {
+        frame->GetSizer()->SetItemMinSize(i, wxSize(len, len));
+        frame->GetSizer()->Layout();
+        if (i % max_len >= system.get_length() || i / max_len >= system.get_length()) {
+            frame->GetSizer()->Show(i, false);
+        } else {
+            frame->GetSizer()->Show(i, true);
+        }
+    }
+}
+
+LightsOutWxWidgets::LightsOutWxWidgets(){
+    // フレームを作成
+    frame = new wxFrame(NULL, wxID_ANY, "Lights Out");
+
+    // 盤面を作成
+    wxFlexGridSizer *grid_sizer = new wxFlexGridSizer(max_len, max_len, 0, 0);
+    for (int i = 0; i < max_size; ++i) {
+        wxButton *button = new wxButton(frame, wxID_ANY);
+        button->SetMinSize(wxSize(50, 50));
+        grid_sizer->Add(button, wxEXPAND | wxSHAPED);
+    }
+    frame->SetSizer(grid_sizer);
+    frame->SetSize(500, 500);
+    NewGame(5);
+
     frame->Show();
+}
+
+LightsOutWxWidgets::~LightsOutWxWidgets(){
+    frame->Destroy();
+}
+
+void LightsOutWxWidgets::NewGame(std::uint8_t length) noexcept{
+    system.set_length(length);
+    Fit();
+    system.random();
+};
+
+void LightsOutWxWidgets::Display() noexcept{
+    return;
+}
+
+bool LightsOutWxWidgets::Push(std::uint16_t position) noexcept{
+    return false;
+}
+
+bool LightsOutWxWidgets::Push(std::uint8_t row, std::uint8_t column) noexcept{
+    return false;
+}
+
+bool LightsOutWxWidgets::IsSuccess() noexcept{
+    return false;
+}
+
+bool LightsOutApp::OnInit(){
+    LightsOutWxWidgets *gui = new LightsOutWxWidgets();
     return true;
 }
