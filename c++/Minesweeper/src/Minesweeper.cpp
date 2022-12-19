692f7945c29f9759ad5ba2be6b13622bf1cfdaa6
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-19 15:51:44 +0900

    MinesweeperSysの盤面の大きさの変更を関数として独立

--- c++/Minesweeper/src/Minesweeper.cpp
+++ c++/Minesweeper/src/Minesweeper.cpp
@@ -9,14 +9,20 @@ private:
 
     // 盤面
     std::vector<bool> board;
-public:
-    MinesweeperSys(std::uint8_t row_number, std::uint8_t column_number){
+
+    // 大きさを変更
+    void SetSize(std::uint8_t row_number, std::uint8_t column_number){
         row = row_number;
         column = column_number;
         board.resize(row * column);
         for (auto v : board) {
             v = false;
         }
+    }
+public:
+    MinesweeperSys(std::uint8_t row_number, std::uint8_t column_number){
+        // 盤面の大きさを設定
+        SetSize(row_number, column_number);
     }
 };
 
