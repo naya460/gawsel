b9d5e09be40dca06baf06794590b534c2195f2d7
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-21 18:23:01 +0900

    MinesweeperSysで爆弾の数を保存

--- c++/Minesweeper/src/MinesweeperSys.cpp
+++ c++/Minesweeper/src/MinesweeperSys.cpp
@@ -1,5 +1,7 @@
 #include "MinesweeperSys.hpp"
 
+#include <algorithm>
+
 // === MinesweeperCell ===
 void MinesweeperCell::Reset(CellData data){
     open = false;
@@ -19,18 +21,20 @@ void MinesweeperCell::Open(){
 }
 
 // === MinesweeperSys ===
-void MinesweeperSys::SetSize(std::uint8_t row_number, std::uint8_t column_number){
+MinesweeperSys::MinesweeperSys(std::uint8_t row_number, std::uint8_t column_number, std::uint16_t mine){
+    // 盤面の大きさを設定
+    SetSizeAndMine(row_number, column_number, mine);
+}
+
+void MinesweeperSys::SetSizeAndMine(std::uint8_t row_number, std::uint8_t column_number, std::uint16_t mine) noexcept{
     // 大きさを保存
     row = row_number;
     column = column_number;
+    // 爆弾の数を変更
+    this->mine = std::min(mine, static_cast<std::uint16_t>(row * column - 9));
     // 盤面の大きさを変更
     board.resize(row * column);
     for (auto v : board) {
         v.Reset(CellData::_0);
     }
-}
-
-MinesweeperSys::MinesweeperSys(std::uint8_t row_number, std::uint8_t column_number){
-    // 盤面の大きさを設定
-    SetSize(row_number, column_number);
 }
