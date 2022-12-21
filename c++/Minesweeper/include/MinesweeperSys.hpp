c98eeb7a404a3bdf78f22a8f819b3766891749cc
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-21 18:06:10 +0900

    MinesweeperCellで入力できる数値を限定

--- c++/Minesweeper/include/MinesweeperSys.hpp
+++ c++/Minesweeper/include/MinesweeperSys.hpp
@@ -3,19 +3,27 @@
 #include <cstdint>
 #include <vector>
 
+// 番号や爆弾の一覧
+enum class CellData {
+    Mine = -1,
+    _0 = 0,
+    _1, _2, _3, _4,
+    _5, _6, _7, _8,
+};
+
 class MinesweeperCell {
 private:
-    bool open = false;      // セルが空いているか
-    std::uint8_t num = 0;   // セルの番号
+    bool open = false;              // セルが空いているか
+    CellData data = CellData::_0;   // 番号や爆弾
 public:
     // コンストラクタ
     MinesweeperCell() = default;
 
     // セルの初期化
-    void Reset(std::uint8_t num);
+    void Reset(CellData data);
 
-    // 数字を取得
-    std::uint8_t GetNum();
+    // 番号や爆弾を返す
+    CellData GetData();
 
     // 空いているか確認
     bool IsOpen();
