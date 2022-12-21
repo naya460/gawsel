bacd70b3d363f39011983f08393324e273958c59
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-21 17:47:01 +0900

    セルをMinesweeperCellとして独立

--- c++/Minesweeper/include/MinesweeperSys.hpp
+++ c++/Minesweeper/include/MinesweeperSys.hpp
@@ -3,6 +3,27 @@
 #include <cstdint>
 #include <vector>
 
+class MinesweeperCell {
+private:
+    bool open = false;      // セルが空いているか
+    std::uint8_t num = 0;   // セルの番号
+public:
+    // コンストラクタ
+    MinesweeperCell() = default;
+
+    // セルの初期化
+    void Reset(std::uint8_t num);
+
+    // 数字を取得
+    std::uint8_t GetNum();
+
+    // 空いているか確認
+    bool IsOpen();
+
+    // 開ける
+    void Open();
+};
+
 class MinesweeperSys {
 private:
     // 盤面の行数と列数
@@ -10,7 +31,7 @@ private:
     std::uint8_t column;
 
     // 盤面
-    std::vector<bool> board;
+    std::vector<MinesweeperCell> board;
 
     // 大きさを変更
     void SetSize(std::uint8_t row_number, std::uint8_t column_number);
--- c++/Minesweeper/src/MinesweeperSys.cpp
+++ c++/Minesweeper/src/MinesweeperSys.cpp
@@ -1,11 +1,32 @@
 #include "MinesweeperSys.hpp"
 
+// === MinesweeperCell ===
+void MinesweeperCell::Reset(std::uint8_t num){
+    open = false;
+    this->num = num;
+}
+
+std::uint8_t MinesweeperCell::GetNum(){
+    return num;
+}
+
+bool MinesweeperCell::IsOpen(){
+    return open;
+}
+
+void MinesweeperCell::Open(){
+    open = true;
+}
+
+// === MinesweeperSys ===
 void MinesweeperSys::SetSize(std::uint8_t row_number, std::uint8_t column_number){
+    // 大きさを保存
     row = row_number;
     column = column_number;
+    // 盤面の大きさを変更
     board.resize(row * column);
     for (auto v : board) {
-        v = false;
+        v.Reset(0);
     }
 }
 
