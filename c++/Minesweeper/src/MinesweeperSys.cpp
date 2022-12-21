06a09defd236e98c5b982f923d7a4dde35df3bfb
naya460 <76655448+naya460@users.noreply.github.com>
2022-12-21 18:37:42 +0900

    MinesweeperSysでセルの情報を取得

--- c++/Minesweeper/src/MinesweeperSys.cpp
+++ c++/Minesweeper/src/MinesweeperSys.cpp
@@ -28,13 +28,21 @@ MinesweeperSys::MinesweeperSys(std::uint8_t row_number, std::uint8_t column_numb
 
 void MinesweeperSys::SetSizeAndMine(std::uint8_t row_number, std::uint8_t column_number, std::uint16_t mine) noexcept{
     // 大きさを保存
-    row = row_number;
-    column = column_number;
+    row_num = row_number;
+    column_num = column_number;
     // 爆弾の数を変更
-    this->mine = std::min(mine, static_cast<std::uint16_t>(row * column - 9));
+    this->mine = std::min(mine, static_cast<std::uint16_t>(row_num * column_num - 9));
     // 盤面の大きさを変更
-    board.resize(row * column);
+    board.resize(row_num * column_num);
     for (auto v : board) {
         v.Reset(CellData::_0);
     }
+}
+
+MinesweeperCell MinesweeperSys::GetCell(std::uint16_t pos) noexcept{
+    return board[pos];
+}
+
+MinesweeperCell MinesweeperSys::GetCell(std::uint8_t row, std::uint8_t column) noexcept{
+    return GetCell(column * column_num + row);
 }
