24b9a78776a4fbc311ef396c59efb3daef53e977
naya460 <76655448+naya460@users.noreply.github.com>
2023-02-22 23:45:57 +0900

    MinesweeperSys内で、GetSize関数を活用

--- c++/Minesweeper/src/MinesweeperSys.cpp
+++ c++/Minesweeper/src/MinesweeperSys.cpp
@@ -154,9 +154,9 @@ void MinesweeperSys::SetSizeAndMine(std::uint8_t row_number, std::uint8_t column
     row_num = row_number;
     column_num = column_number;
     // 爆弾の数を変更
-    this->mine = std::min(mine, static_cast<std::uint16_t>(row_num * column_num - 9));
+    this->mine = std::min(mine, static_cast<std::uint16_t>(GetSize() - 9));
     // 盤面の大きさを変更
-    board.resize(row_num * column_num);
+    board.resize(GetSize());
     Reset();
 }
 
