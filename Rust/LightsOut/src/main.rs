efd50e30a3baefff37c3a71008c8013ace2f4748
naya460 <76655448+naya460@users.noreply.github.com>
2023-02-28 23:20:44 +0900

    <Rust/LightsOut>LightsOutSysを作成

--- Rust/LightsOut/src/main.rs
+++ Rust/LightsOut/src/main.rs
@@ -1,3 +1,15 @@
+struct LightsOutSys {
+    length: u8,         // 一辺のライトの数
+    board: Vec<bool>,   // 盤面
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut sys = LightsOutSys {
+        length: 3u8,
+        board: Vec::new(),
+    };
+    println!("{}", sys.length);
+    for _i in 0..sys.length {
+        sys.board.push(false);
+    }
 }
