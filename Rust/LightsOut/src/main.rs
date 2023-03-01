6d61e1f06fc738ee840edf6452bf1ffca46f4c09
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-01 13:28:07 +0900

    <Rust/LightsOut>LightsOutSys構造体のフィールドへのアクセスを関数化

--- Rust/LightsOut/src/main.rs
+++ Rust/LightsOut/src/main.rs
@@ -3,8 +3,8 @@ use crate::lights_out_sys::LightsOutSys;
 
 fn main() {
     let sys = LightsOutSys::create(3);
-    println!("{}", sys.length);
-    for v in sys.board {
-        println!("{}", v);
+    println!("{}", sys.get_length());
+    for i in 0..sys.get_size() {
+        println!("{}", sys.is_on(i));
     }
 }
