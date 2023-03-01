7d43ff4d769caa88fe5200bb29e7b5bf83ff05d3
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-01 22:29:06 +0900

    <Rust/LightsOut>LightsOutSysにライトを押す関数を作成

--- Rust/LightsOut/src/main.rs
+++ Rust/LightsOut/src/main.rs
@@ -2,7 +2,8 @@ mod lights_out_sys;
 use crate::lights_out_sys::LightsOutSys;
 
 fn main() {
-    let sys = LightsOutSys::create(3);
+    let mut sys = LightsOutSys::create(3);
+    sys.push(5).unwrap();
     println!("{}", sys.get_length());
     for i in 0..sys.get_size() {
         println!("{}", sys.is_on(i).unwrap());
