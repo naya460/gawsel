457ca7f7efc842ab15a569a32f779816c0fb0601
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-01 10:05:33 +0900

    <Rust/LightsOut>LightsOutSysに大きさを変更する関数を実装

--- Rust/LightsOut/src/main.rs
+++ Rust/LightsOut/src/main.rs
@@ -1,17 +1,24 @@
 struct LightsOutSys {
     length: u8,         // 一辺のライトの数
+    size: u16,          // ライトの数
     board: Vec<bool>,   // 盤面
 }
 
 impl LightsOutSys {
     // 盤面の作成
     fn create(length: u8) -> LightsOutSys {
-        let board = vec![false; (length * length).into()];
-        let sys = LightsOutSys {
-            length,
-            board,
-        };
+        let mut sys = LightsOutSys { length, size: 0, board: Vec::new() };
+        sys.set_length(length);
         sys
+    }
+}
+
+impl LightsOutSys {
+    // 大きさを変更
+    fn set_length(&mut self, length: u8) {
+        self.length = length;
+        self.size = (length as u16).pow(2);
+        self.board.resize(self.size.into(), false);
     }
 }
 
