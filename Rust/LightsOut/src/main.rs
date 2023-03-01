7b4c31733509906053bb7261a1cdb2ea7446a092
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-01 10:30:23 +0900

    <Rust/LightsOut>LightsOutSysを別のファイルに分割

--- Rust/LightsOut/src/main.rs
+++ Rust/LightsOut/src/main.rs
@@ -1,26 +1,5 @@
-struct LightsOutSys {
-    length: u8,         // 一辺のライトの数
-    size: u16,          // ライトの数
-    board: Vec<bool>,   // 盤面
-}
-
-impl LightsOutSys {
-    // 盤面の作成
-    fn create(length: u8) -> LightsOutSys {
-        let mut sys = LightsOutSys { length, size: 0, board: Vec::new() };
-        sys.set_length(length);
-        sys
-    }
-}
-
-impl LightsOutSys {
-    // 大きさを変更
-    fn set_length(&mut self, length: u8) {
-        self.length = length;
-        self.size = (length as u16).pow(2);
-        self.board.resize(self.size.into(), false);
-    }
-}
+mod lights_out_sys;
+use crate::lights_out_sys::LightsOutSys;
 
 fn main() {
     let sys = LightsOutSys::create(3);
