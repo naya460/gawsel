c1accdb2d14252617c00deee13f65b65c6cd4e6f
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-20 20:42:44 +0900

    <Next.js/LightsOut>LightsOutBoardでLightsOutCellを並べる

--- Next.js/LightsOut/pages/index.tsx
+++ Next.js/LightsOut/pages/index.tsx
@@ -6,10 +6,34 @@ const LightsOutCell = () => {
   )
 }
 
+const LightsOutBoard = () => {
+  function line() {
+    const list = [];
+    for (let i = 0; i < 3; i++) {
+      list.push(<LightsOutCell />);
+    }
+    return <div>{list}</div>;
+  }
+
+  return (
+    <div>
+      {
+        function () {
+          const list = [];
+          for (let i = 0; i < 3; i++) {
+            list.push(line());
+          }
+          return <div>{list}</div>;
+        }()
+      }
+    </div>
+  );
+}
+
 export default function IndexPage() {
   return (
     <div>
-      <LightsOutCell />
+      <LightsOutBoard />
     </div>
   )
 }
