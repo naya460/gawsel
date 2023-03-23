2c3ab190d682e3954b5320eebaea3126719d8fc9
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-23 23:18:08 +0900

    <Next.js/LightsOut>LightsOutBoardに関するcssを別のモジュールに分割

--- Next.js/LightsOut/components/LightsOutBoard.tsx
+++ Next.js/LightsOut/components/LightsOutBoard.tsx
@@ -1,40 +1,40 @@
-import React, {useState} from "react"
+import React from "react"
 
-import styles from "../pages/LightsOut.module.css";
+import styles from "./LightsOutBoard.module.css";
 
 import LightsOutCell from "./LightsOutCell";
 
 export default function LightsOutBoard(props) {
-    function handleClick(i) {
-      let lights_tmp = props.lights.slice();
-      lights_tmp = props.pushLight(lights_tmp, i);
-      props.setLights(lights_tmp);
-    }
-  
-    function line(row, length) {
-      const list = [];
-      for (let i = 0; i < length; i++) {
-        list.push(
-          <LightsOutCell
-            isLightOn={props.lights[row * props.length + i]}
-            onClick={() => handleClick(row * props.length + i)}
-          />
-        );
-      }
-      return <div className={styles.LightsOutBoardLine}>{list}</div>;
-    }
-  
-    return (
-      <div>
-        {
-          function () {
-            const list = [];
-            for (let i = 0; i < props.length; i++) {
-              list.push(line(i, props.length));
-            }
-            return <div className={styles.LightsOutBoard}>{list}</div>;
-          }()
-        }
-      </div>
-    );
+  function handleClick(i) {
+    let lights_tmp = props.lights.slice();
+    lights_tmp = props.pushLight(lights_tmp, i);
+    props.setLights(lights_tmp);
   }
+
+  function line(row, length) {
+    const list = [];
+    for (let i = 0; i < length; i++) {
+      list.push(
+        <LightsOutCell
+          isLightOn={props.lights[row * props.length + i]}
+          onClick={() => handleClick(row * props.length + i)}
+        />
+      );
+    }
+  return <div className={styles.line}>{list}</div>;
+}
+  
+  return (
+    <div>
+      {
+        function () {
+          const list = [];
+          for (let i = 0; i < props.length; i++) {
+            list.push(line(i, props.length));
+          }
+          return <div className={styles.board}>{list}</div>;
+        }()
+      }
+    </div>
+  );
+}
