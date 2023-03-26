e4932615d2bb73b70c4d355e2fee83d97dcfe021
naya460 <76655448+naya460@users.noreply.github.com>
2023-03-26 23:43:59 +0900

    <Next.js/LightsOut>クリアしたか判定する処理を作成

--- Next.js/LightsOut/components/LightsOutGame/LightsOutGame.tsx
+++ Next.js/LightsOut/components/LightsOutGame/LightsOutGame.tsx
@@ -8,14 +8,33 @@ import LightsOutMenuBar from "../LightsOutMenuBar/LightsOutMenuBar"
 export default function LightsOutGame() {
   const [lights, setLights] = useState(Array(9).fill(false));
   const [length, setLength] = useState(3);
+  const [clear, setClear] = useState(true);
+
+  // 全てのライトがoffになったか確認
+  function checkLights(lights: Array<boolean>) : boolean {
+    if (clear) return false;
+    for (let i = 0; i < length**2; i++) {
+      if (lights[i] == true) {
+        return false;
+      }
+    }
+    setClear(true);
+    return true;
+  }
 
   // ライトを押したときの処理
   function pushLight(lights: Array<boolean>, pos: number) {
+    // ライトを反転
     lights[pos] = !lights[pos];
     if (pos >= length)              lights[pos - length] = !lights[pos - length];
     if (pos + length <= length**2)  lights[pos + length] = !lights[pos + length];
     if (pos % length != 0)          lights[pos - 1] = !lights[pos - 1];
     if (pos % length < length - 1)  lights[pos + 1] = !lights[pos + 1];
+    // クリアしたか確認
+    if (checkLights(lights)) {
+      console.log("Clear");
+    }
+
     return lights;
   }
 
@@ -29,6 +48,7 @@ export default function LightsOutGame() {
       }
     }
     setLights(lights_tmp);
+    setClear(false);
   }
 
   // 盤面の大きさを変えたときの処理
