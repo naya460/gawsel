f7518f3e13e709d01db1e5353e5de5250ad5f5da
naya460 <76655448+naya460@users.noreply.github.com>
2023-04-08 21:40:44 +0900

    <Next.js/Minesweeper>セルを右クリックで旗を立てる

--- Next.js/Minesweeper/components/MinesweeperBoard.tsx
+++ Next.js/Minesweeper/components/MinesweeperBoard.tsx
@@ -7,25 +7,39 @@ import MinesweeperCell from "./MinesweeperCell"
 type CellStatus = {
   isOpen: boolean;
   number: number; // -1のとき爆弾
+  isFlagged: boolean;
 }
 
 export default function(): react.ReactElement {
   const [lx, setLx] = useState(30);
   const [ly, setLy] = useState(16);
   const [board, setBoard] = useState<Array<CellStatus>>(
-    Array<CellStatus>(lx * ly).fill({isOpen: false, number: 1})
+    Array<CellStatus>(lx * ly).fill({isOpen: false, number: 1, isFlagged: false})
   );
 
   function handleClickCell(x: number, y: number) {
     const board_slice = board.slice();
     const pos = lx * y + x;
+    // 旗が立っているとき開けない
+    if (board_slice[pos].isFlagged == true) return;
+    // 開ける
     board_slice[pos] = {...board_slice[pos], isOpen: true};
+    setBoard(board_slice);
+  }
+
+  function handleRightClickCell(x: number, y: number) {
+    const board_slice = board.slice();
+    const pos = lx * y + x;
+    // 開いているとき、旗を立てない
+    if (board_slice[pos].isOpen == true) return;
+    // 旗を立てる
+    board_slice[pos] = {...board_slice[pos], isFlagged: !board_slice[pos].isFlagged}
     setBoard(board_slice);
   }
 
   return (
     <>
-      <div>
+      <div onContextMenu={(event) => event.preventDefault()}>
         {
           function (): react.ReactElement {
             const list = [];
@@ -37,7 +51,9 @@ export default function(): react.ReactElement {
                   <MinesweeperCell
                     isOpen={board[pos].isOpen}
                     number={board[pos].number}
+                    isFlagged={board[pos].isFlagged}
                     onClick={() => handleClickCell(j, i)}
+                    onRightClick={() => handleRightClickCell(j, i)}
                     key={pos}
                   />
                 );
--- Next.js/Minesweeper/components/MinesweeperCell.tsx
+++ Next.js/Minesweeper/components/MinesweeperCell.tsx
@@ -5,22 +5,36 @@ import styles from "./MinesweeperCell.module.css"
 type Props = {
   isOpen: boolean;
   number: number;
+  isFlagged: boolean;
   onClick: () => void;
+  onRightClick: () => void;
 }
 
 export default function MinesweeperCell(props: Props): react.ReactElement {
+  // 表示する内容を返却
   function displayNumber(): string {
+    if (props.isFlagged == true) {
+      return 'P';
+    }
     if (props.number == 0) {
       return '';
-    } else if (props.isOpen) {
+    }
+    if (props.isOpen) {
       return props.number.toString();
     }
   }
 
   return (
     <button
-      className={`${styles.cell} ${props.isOpen? styles.cell_open : styles.cell_close}`}
+      className={`
+        ${styles.cell} 
+        ${props.isFlagged?
+          styles.cell_flagged :
+          (props.isOpen? styles.cell_open : styles.cell_close)
+        }
+      `}
       onClick={() => props.onClick()}
+      onContextMenu={() => {props.onRightClick()}}
     >{displayNumber()}</button>
   );
 }
