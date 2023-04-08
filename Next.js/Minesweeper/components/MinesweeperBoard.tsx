f6e7f030f714921035f780db199b3ef4a83583be
naya460 <76655448+naya460@users.noreply.github.com>
2023-04-08 17:22:35 +0900

    <Next.js/Minesweeper>セルを開けられるように変更

--- Next.js/Minesweeper/components/MinesweeperBoard.tsx
+++ Next.js/Minesweeper/components/MinesweeperBoard.tsx
@@ -1,20 +1,37 @@
-import react from "react"
+import react, {useState} from "react"
 
 import styles from "./MinesweeperBoard.module.css"
 
 import MinesweeperCell from "./MinesweeperCell"
 
 export default function(): react.ReactElement {
+  const [lx, setLx] = useState(30);
+  const [ly, setLy] = useState(16);
+  const [board, setBoard] = useState(Array(lx * ly).fill(false));
+
+  function handleClickCell(x: number, y: number) {
+    const tmp = board.slice();
+    tmp[lx * y + x] = true;
+    setBoard(tmp);
+  }
+
   return (
     <>
       <div>
         {
           function (): react.ReactElement {
             const list = [];
-            for (let i = 0; i < 16; i++) {
+            for (let i = 0; i < ly; i++) {
               const line = [];
-              for (let j = 0; j < 30; j++) {
-                line.push(<MinesweeperCell />);
+              for (let j = 0; j < lx; j++) {
+                const pos = lx * i + j;
+                line.push(
+                  <MinesweeperCell
+                    status={board[pos]}
+                    onClick={() => handleClickCell(j, i)}
+                    key={pos}
+                  />
+                );
               }
               list.push(<div className={styles.line}>{line}</div>);
             }
