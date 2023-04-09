5b87e6eacd78870958f1a527833d5ff7cee4e295
naya460 <76655448+naya460@users.noreply.github.com>
2023-04-09 20:26:31 +0900

    <Next.js/Minesweeper>空のセルを押したとき、周りのセルも自動的に開放

--- Next.js/Minesweeper/components/MinesweeperBoard.tsx
+++ Next.js/Minesweeper/components/MinesweeperBoard.tsx
@@ -25,14 +25,40 @@ export default function(): react.ReactElement {
     const pos = lx * y + x;
     // 旗が立っているとき開けない
     if (board_slice[pos].isFlagged == true) return;
+    // 開けているとき処理をしない
+    if (board_slice[pos].isOpen == true) return;
     // 最初に開けるときに初期化
     if (start == false) {
       board_slice = handleRandomize(x, y);
       setStart(true);
     }
     // 開ける
-    board_slice[pos] = {...board_slice[pos], isOpen: true};
+    board_slice = openCell(board_slice, x, y);
     setBoard(board_slice);
+  }
+
+  // セルを開ける
+  function openCell(board_slice: CellStatus[], x: number, y: number): CellStatus[] {
+    const pos = lx * y + x;
+    // 開いているとき返却
+    if (board_slice[pos].isOpen == true) return board_slice;
+    // 開ける
+    board_slice[pos] = {...board_slice[pos], isOpen: true};
+    if (board_slice[pos].number == 0) {
+      if (checkUpper(pos)) {  // 上側
+        if (checkLeft(pos)) board_slice = openCell(board_slice, x - 1, y - 1);    // 左上
+        board_slice = openCell(board_slice, x, y - 1);   // 上
+        if (checkRight(pos)) board_slice = openCell(board_slice, x + 1, y - 1);   // 右上
+      }
+      if (checkBottom(pos)) {
+        if (checkLeft(pos)) board_slice = openCell(board_slice, x - 1, y + 1);    // 左下
+        board_slice = openCell(board_slice, x, y + 1);  // 下
+        if (checkRight(pos)) board_slice = openCell(board_slice, x + 1, y + 1);   // 右下
+      }
+      if (checkLeft(pos)) board_slice = openCell(board_slice, x - 1, y);    // 左
+      if (checkRight(pos)) board_slice = openCell(board_slice, x + 1, y);   // 右
+    }
+    return board_slice;
   }
 
   // セルを右クリックしたとき
@@ -46,25 +72,26 @@ export default function(): react.ReactElement {
     setBoard(board_slice);
   }
 
+  // 方向が存在するか確認する関数
+  function checkUpper(pos: number): boolean {
+    return (pos / lx) >= 1;
+  }
+  function checkBottom(pos: number): boolean {
+    return (pos / lx) < ly - 1;
+  }
+  function checkLeft(pos: number): boolean {
+    return (pos % lx) >= 1;
+  }
+  function checkRight(pos: number): boolean {
+    return (pos % lx) < lx - 1;
+  }
+
   // ランダムに爆弾を生成
   function handleRandomize(x: number, y: number): CellStatus[] {
     // 番号の一覧を作成
     let number_list = Array<number>(lx * ly);
     for (let i = 0; i < number_list.length; i++) {
       number_list[i] = i;
-    }
-    // 方向が存在するか確認する関数
-    function checkUpper(pos: number): boolean {
-      return (pos / lx) >= 1;
-    }
-    function checkBottom(pos: number): boolean {
-      return (pos / lx) < ly - 1;
-    }
-    function checkLeft(pos: number): boolean {
-      return (pos % lx) >= 1;
-    }
-    function checkRight(pos: number): boolean {
-      return (pos % lx) < lx - 1;
     }
     // 数字を増やす関数
     function addNumber(pos: number): void {
@@ -75,7 +102,7 @@ export default function(): react.ReactElement {
     }
     // ランダムに配置
     let board_slice = board.slice();
-    board_slice.fill({isOpen: true, number: 0, isFlagged: false});
+    board_slice.fill({isOpen: false, number: 0, isFlagged: false});
     for (let i = 0; i < mine; i++) {
       // 爆弾を配置
       let pos = number_list[Math.floor(Math.random() * number_list.length)];
