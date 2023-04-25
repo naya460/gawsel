75673669eb3a938bee2da66f60d9429c399eb601
naya460 <76655448+naya460@users.noreply.github.com>
2023-04-25 17:11:01 +0900

    <c++/SlidePuzzle>SlidePuzzleSysクラスで長さと盤面を管理

--- c++/SlidePuzzle/src/SlidePuzzle.cpp
+++ c++/SlidePuzzle/src/SlidePuzzle.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 
+#include "SlidePuzzleSys.hpp"
+
 int main(void) {
-    std::cout << "Test" << std::endl;
+    SlidePuzzleSys sys;
+    for (int i = 0; i < sys.get_size(); ++i) {
+        printf("%2d ", sys.get_number(i));
+        if ((i + 1) % sys.get_length() == 0) {
+            putchar('\n');
+        }
+    }
     return 0;
 }
