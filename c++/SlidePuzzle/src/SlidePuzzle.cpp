da949d3967c15a6d99caa15f115b8ee565637f3a
naya460 <76655448+naya460@users.noreply.github.com>
2023-04-26 22:44:30 +0900

    <c++/SlidePuzzle>SlidePuzzleSysクラスで数字を移動

--- c++/SlidePuzzle/src/SlidePuzzle.cpp
+++ c++/SlidePuzzle/src/SlidePuzzle.cpp
@@ -4,6 +4,10 @@
 
 int main(void) {
     SlidePuzzleSys sys;
+    sys.move_number(11);
+    sys.move_number(10);
+    sys.move_number(14);
+    sys.move_number(15);
     for (int i = 0; i < sys.get_size(); ++i) {
         printf("%2d ", sys.get_number(i));
         if ((i + 1) % sys.get_length() == 0) {
