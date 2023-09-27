1fdd733518f4f685a78fb6511557cdbb4b26e5c2
naya460 <76655448+naya460@users.noreply.github.com>
2023-09-27 22:22:11 +0900

    <website>Gamesドロップダウンを作成

--- website/src/components/website/header/menu_wide.tsx
+++ website/src/components/website/header/menu_wide.tsx
@@ -1,12 +1,31 @@
+import { useState } from 'react'
+import Link from 'next/link'
+
 import styles from './menu_wide.module.css'
 
 export default function MenuWide(props: {className? : string | undefined}) {
+  const [isGameMenuOpen, setGameMenuOpen] = useState(false);
+
 	return (
 		<div className={props.className}>
-      <div className={styles.games}>
+      <div
+        className={styles.games}
+        onClick={() => setGameMenuOpen(!isGameMenuOpen)}
+      >
         Games
         <span className="material-symbols-outlined" style={{width: 24}}>expand_more</span>
 		  </div>
+      <div
+        className={`${styles.background} ${!isGameMenuOpen && styles.background_hidden}`}
+        onClick={() => setGameMenuOpen(false)}
+      />
+      <div
+        className={`${styles.games_menu} ${!isGameMenuOpen && styles.games_menu_hidden}`}
+        onClick={() => setGameMenuOpen(!isGameMenuOpen)}
+      >
+        <Link href="/play/minesweeper">Minesweeper</Link><br />
+        <Link href="/play/lightsout">LightsOut</Link>
+      </div>
 		</div>
 	);
 }
